229cdc6aa39e9e698b1b560c87767e22008a5206
Thomas Klepzig <tklepzig@users.noreply.github.com>
2016-04-29 20:39:49 +0200

    Added files via upload

---
+++ git-status-all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+dir=${1-.}
+
+find $dir -maxdepth 1 -mindepth 1 -type d -exec sh -c "test -d \"{}/.git\" && (echo \"--------------------------------\" && echo \"{}\" && cd \"{}\" && git status -sb && echo && echo)" \;
