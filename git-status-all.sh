01bd881b9713e9af46e24a8f74d588b03fb315e8
Thomas Klepzig <tklepzig@users.noreply.github.com>
2016-04-30 18:24:54 +0200

    Update git-status-all.sh

--- git-status-all.sh
+++ git-status-all.sh
@@ -2,4 +2,4 @@
 
 dir=${1-.}
 
-find $dir -maxdepth 1 -mindepth 1 -type d -exec sh -c "test -d \"{}/.git\" && (echo \"--------------------------------\" && echo \"{}\" && cd \"{}\" && git status -sb && echo && echo)" \;
+find $dir -maxdepth 1 -mindepth 1 -type d -exec sh -c "test -d \"{}/.git\" && (echo \"--------------------------------\" && echo \"{}\" && cd \"{}\" && git status -sb && echo && echo \"Branches:\" && git branch -vv && echo && echo)" \;
