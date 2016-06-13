2222690eb649defbbf393b6c03067fe579a1ed16
Thomas Klepzig <tklepzig@users.noreply.github.com>
2016-06-13 13:16:18 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -266,6 +266,7 @@ setGitAliase()
     git config --global alias.rbc "git rebase --continue"
     git config --global alias.undo "!f() { git reset --hard \$1 && git clean -df \$1; }; f"
     git config --global alias.frb "!f() { git fetch && git rebase; }; f"
+    git config --global alias.dummy "commit --allow-empty -m 'dummy commit, contains no change'"
 }
 
 setProMode $1
