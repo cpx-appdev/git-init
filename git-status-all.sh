e6e51c39e89b552af48616cd2e69a9d8be81680f
Thomas <Thomas@DevBook>
2016-11-10 20:38:06 +0100

    removed git status all (will be included in setup)

--- git-status-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dir=${1-.}
-
-find $dir -maxdepth 1 -mindepth 1 -type d -exec sh -c "test -d \"{}/.git\" && (echo \"--------------------------------\" && echo \"{}\" && cd \"{}\" && git status -sb && echo && echo \"Branches:\" && git branch -vv && echo && echo)" \;
